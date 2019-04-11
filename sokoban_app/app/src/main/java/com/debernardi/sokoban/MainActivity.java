b0e203f74842d32d363e1ecb3e173d95f061f288
Jelmer Firet <jfiret@eljakim.nl>
2019-04-11 20:09:50 +0200

    Add (partially temporary) navigation functionality

--- sokoban_app/app/src/main/java/com/debernardi/sokoban/MainActivity.java
+++ sokoban_app/app/src/main/java/com/debernardi/sokoban/MainActivity.java
@@ -1,7 +1,9 @@
 package com.debernardi.sokoban;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickLevelSelect(View view){
+        Intent startLevelSelect = new Intent(MainActivity.this, LevelSelect.class);
+        startActivity(startLevelSelect);
+    }
+
+    public void onClickStatistics(View view){
+        Intent startStatistics = new Intent(MainActivity.this, Statistics.class);
+        startActivity(startStatistics);
+    }
+
+    public void onClickSettings(View view){
+        Intent startSettings = new Intent(MainActivity.this, Settings.class);
+        startActivity(startSettings);
     }
 }
