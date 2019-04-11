f38f9c4433369821d11bed1b2ca99f8c8aaba100
Jelmer Firet <spaanse.aak@gmail.com>
2019-04-11 20:09:50 +0200

    Add (partially temporary) navigation functionality

--- sokoban_app/app/src/main/java/com/debernardi/sokoban/LevelSelect.java
+++ sokoban_app/app/src/main/java/com/debernardi/sokoban/LevelSelect.java
@@ -1,7 +1,9 @@
 package com.debernardi.sokoban;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LevelSelect extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class LevelSelect extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_level_select);
+    }
+
+    public void onClickGame(View view){
+        Intent startGame = new Intent(LevelSelect.this, Game.class);
+        startActivity(startGame);
     }
 }
