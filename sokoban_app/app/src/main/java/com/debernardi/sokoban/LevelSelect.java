78d4be9af607f8b6f62278626ca5b58e41f0a852
Jelmer Firet <spaanse.aak@gmail.com>
2019-04-12 12:08:54 +0200

    add code to generate a level select menu

--- sokoban_app/app/src/main/java/com/debernardi/sokoban/LevelSelect.java
+++ sokoban_app/app/src/main/java/com/debernardi/sokoban/LevelSelect.java
@@ -4,6 +4,10 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class LevelSelect extends AppCompatActivity {
 
@@ -11,6 +15,27 @@ public class LevelSelect extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_level_select);
+        LinearLayout layLevels = (LinearLayout) findViewById(R.id.layLevels);
+        for (int i = 0;i<5;i++){
+            LinearLayout level = new LinearLayout(this);
+            level.setOrientation(LinearLayout.HORIZONTAL);
+
+            ImageView preview = new ImageView(this);
+            preview.setImageResource(R.color.colorPrimary);
+
+            TextView levelName = new TextView(this);
+            levelName.setText(String.format("Level: %d", i));
+            levelName.setTextSize(18);
+
+            TextView bestText = new TextView(this);
+            bestText.setText(String.format("%d/%d",0,0));
+            bestText.setTextSize(12);
+
+            level.addView(preview);
+            level.addView(levelName);
+            level.addView(bestText);
+            layLevels.addView(level);
+        }
     }
 
     public void onClickGame(View view){
