99f310c6ac476db09867691ecb92e57206931e0e
Jelmer Firet <spaanse.aak@gmail.com>
2019-04-15 22:02:52 +0200

    updated the levelSelect menu

--- sokoban_app/app/src/main/java/com/debernardi/sokoban/LevelSelect.java
+++ sokoban_app/app/src/main/java/com/debernardi/sokoban/LevelSelect.java
@@ -3,10 +3,10 @@ package com.debernardi.sokoban;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.io.BufferedReader;
@@ -32,34 +32,60 @@ public class LevelSelect extends AppCompatActivity {
             layLevels.addView(levelLoadError);
             return;
         }
+        Boolean even = false;
         for (String levelFilename:levelFiles){
-            Log.i("levelFilename",levelFilename);
             String levelName;
             try {
-                BufferedReader levelReader = new BufferedReader(new InputStreamReader(getAssets().open(levelFilename)));
+                BufferedReader levelReader = new BufferedReader(new InputStreamReader(getAssets().open("levels/"+levelFilename)));
                 levelName = levelReader.readLine();
             } catch (IOException e) {
                 continue;
             }
-            LinearLayout level = new LinearLayout(this);
-            level.setOrientation(LinearLayout.HORIZONTAL);
+            RelativeLayout level = new RelativeLayout(this);
+            if (even){
+                level.setBackgroundColor(getResources().getColor(R.color.levelSelectBackgroundEven));
+            }
+            else{
+                level.setBackgroundColor(getResources().getColor(R.color.levelSelectBackgroundOdd));
+            }
+            even = !even;
 
             ImageView previewView = new ImageView(this);
             previewView.setImageResource(R.color.colorPrimary);
+            RelativeLayout.LayoutParams previewViewLp = new RelativeLayout.LayoutParams(
+                    RelativeLayout.LayoutParams.WRAP_CONTENT,
+                    RelativeLayout.LayoutParams.WRAP_CONTENT);
+            previewViewLp.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
+            previewViewLp.addRule(RelativeLayout.CENTER_VERTICAL);
+            previewView.setLayoutParams(previewViewLp);
 
             TextView levelNameView = new TextView(this);
             levelNameView.setText(levelName);
-            levelNameView.setTextSize(18);
+            levelNameView.setTextSize(24);
+            RelativeLayout.LayoutParams levelNameViewLp = new RelativeLayout.LayoutParams(
+                    RelativeLayout.LayoutParams.WRAP_CONTENT,
+                    RelativeLayout.LayoutParams.WRAP_CONTENT);
+            levelNameViewLp.addRule(RelativeLayout.RIGHT_OF,previewView.getId());
+            levelNameViewLp.addRule(RelativeLayout.ALIGN_PARENT_TOP);
+            levelNameView.setLayoutParams(levelNameViewLp);
 
             TextView bestTextView = new TextView(this);
             bestTextView.setText(String.format("%d/%d",0,0));
-            bestTextView.setTextSize(12);
+            bestTextView.setTextSize(18);
+            bestTextView.setPadding(10,3,10,3);
+            RelativeLayout.LayoutParams bestTextViewLp = new RelativeLayout.LayoutParams(
+                    RelativeLayout.LayoutParams.WRAP_CONTENT,
+                    RelativeLayout.LayoutParams.WRAP_CONTENT);
+            bestTextViewLp.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+            bestTextViewLp.addRule(RelativeLayout.CENTER_VERTICAL);
+            bestTextView.setLayoutParams(bestTextViewLp);
+
 
             level.addView(previewView);
             level.addView(levelNameView);
             level.addView(bestTextView);
             level.setClickable(true);
-            final String levelFilenameCpy = levelFilename;
+            final String levelFilenameCpy = "levels/"+levelFilename;
             level.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
