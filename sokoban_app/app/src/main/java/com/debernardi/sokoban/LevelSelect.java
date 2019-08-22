89fc306be2df77ff47c3e1da67105c9aca48a4c0
Jelmer Firet <spaanse.aak@gmail.com>
2019-08-22 19:51:06 +0200

    Maak in de levelselect gebruik van de nieuwe klasses

--- sokoban_app/app/src/main/java/com/debernardi/sokoban/LevelSelect.java
+++ sokoban_app/app/src/main/java/com/debernardi/sokoban/LevelSelect.java
@@ -1,36 +1,26 @@
 package com.debernardi.sokoban;
 
-import android.content.Intent;
 import android.content.SharedPreferences;
-import android.graphics.Bitmap;
-import android.graphics.Point;
-import android.support.v4.content.res.ResourcesCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Display;
-import android.view.View;
-import android.widget.ImageView;
+import android.support.v7.app.AppCompatActivity;
+import android.transition.TransitionManager;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
-
 import org.apache.commons.io.FilenameUtils;
 
-import game.Level;
-import game.Texture;
+import java.util.ArrayList;
 
-import static java.lang.Math.max;
+import game.Texture;
+import view.LevelCollectionItem;
 
 
 public class LevelSelect extends AppCompatActivity {
 
     // numLevelsOnScreen = roughly the number of levels that will fit on a screen before scrolling
     static private int numLevelsOnScreen = 10;
+    static private String baseFilename = "All.sok";
     LinearLayout layLevels;
     ArrayList<String> levelFiles = new ArrayList<>();
 
@@ -47,142 +37,125 @@ public class LevelSelect extends AppCompatActivity {
         setContentView(R.layout.activity_level_select);
         layLevels = findViewById(R.id.layLevels);
 
-        // Fetch filenames of the level files
-        try {
-            levelFiles = new ArrayList<>(Arrays.asList(getAssets().list("levels")));
-            if (levelFiles.size() == 0){
-                throw new IOException();
-            }
-        } catch (IOException e) {
-            TextView levelLoadError = new TextView(this);
-            levelLoadError.setText(getString(R.string.levelLoadError));
-            layLevels.addView(levelLoadError);
-            return;
-        }
+        String[] AllCollection = {"All.sok","","","0"};
 
-        if (!BuildConfig.DEBUG){
-            Iterator<String> it = levelFiles.iterator();
-            while (it.hasNext()){
-                String filename = it.next();
-                if (filename.contains("9999_test")){
-                    it.remove();
-                }
-            }
-        }
+        LevelCollectionItem AllLevels = new LevelCollectionItem(this,AllCollection,layLevels);
+        int backgroundColor = getResources().getColor(R.color.colorPrimaryLight);
+        layLevels.addView(AllLevels.getView(false,0));
 
         // Get screen size
-        Display display = getWindowManager().getDefaultDisplay();
-        Point screenSize = new Point();
-        display.getSize(screenSize);
+//        Display display = getWindowManager().getDefaultDisplay();
+//        Point screenSize = new Point();
+//        display.getSize(screenSize);
 
         // Get highscores shared preferences
-        SharedPreferences prefHighscores = this.getSharedPreferences("Highscores", MODE_PRIVATE);
+//        SharedPreferences prefHighscores = this.getSharedPreferences("Highscores", MODE_PRIVATE);
 
-        int previewSize = max(screenSize.x,screenSize.y)/numLevelsOnScreen;
+//        int previewSize = max(screenSize.x,screenSize.y)/numLevelsOnScreen;
 
-        boolean even = false;
-        for (String levelFilename:levelFiles){
-            // Figure out properties of the level
-            int highscore = prefHighscores.getInt(FilenameUtils.removeExtension(levelFilename),-1);
-            Level levelItem = new Level(this,"levels/"+levelFilename);
-
-            // Make a container for a level
-            RelativeLayout level = new RelativeLayout(this);
-            if (even){
-                level.setBackgroundColor(getResources().getColor(R.color.levelSelectBackgroundEven));
-            }
-            else{
-                level.setBackgroundColor(getResources().getColor(R.color.levelSelectBackgroundOdd));
-            }
-            even = !even;
-
-            // Make preview image
-            ImageView previewView = new ImageView(this);
-            previewView.setId(View.generateViewId());
-            previewView.setAdjustViewBounds(true);
-            try{
-                Bitmap preview = GameView.getLevelBitmap(levelItem,false);
-                previewView.setImageBitmap(Bitmap.createScaledBitmap(preview,previewSize,
-                        preview.getHeight()*previewSize/preview.getWidth(),false));
-            }
-            catch (Exception e){
-                previewView.setImageResource(R.color.colorPrimary);
-            }
-
-            // Make level title text
-            TextView levelNameView = new TextView(this);
-            levelNameView.setText(levelItem.getLevelName());
-            levelNameView.setTextSize(20);
-            levelNameView.setId(View.generateViewId());
-            levelNameView.setTypeface(ResourcesCompat.getFont(this,R.font.dtm_mono));
-
-            // Make level title text
-            TextView authorNameView = new TextView(this);
-            authorNameView.setText(levelItem.getAuthor());
-            authorNameView.setTextSize(16);
-            authorNameView.setId(View.generateViewId());
-            authorNameView.setTypeface(ResourcesCompat.getFont(this,R.font.dtm_mono));
-
-            // Make best score text
-            TextView bestTextView = new TextView(this);
-            bestTextView.setText(String.format("%s/%s",highscore>=0?highscore:"-",
-		    levelItem.getBestPossibleScore()>=0?levelItem.getBestPossibleScore():"-"));
-            bestTextView.setTextSize(16);
-            bestTextView.setPadding(10,3,10,3);
-            bestTextView.setId(View.generateViewId());
-            bestTextView.setTypeface(ResourcesCompat.getFont(this,R.font.dtm_mono));
-
-            // Add elements to level container
-            level.addView(previewView);
-            level.addView(levelNameView);
-            level.addView(authorNameView);
-            level.addView(bestTextView);
-
-            // Add layout rules
-            RelativeLayout.LayoutParams previewViewLp = (RelativeLayout.LayoutParams) previewView.getLayoutParams();
-            previewViewLp.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
-            previewViewLp.addRule(RelativeLayout.CENTER_VERTICAL);
-            previewViewLp.height = max(screenSize.x,screenSize.y)/numLevelsOnScreen;
-            previewViewLp.width = max(screenSize.x,screenSize.y)/numLevelsOnScreen;
-            RelativeLayout.LayoutParams levelNameViewLp = (RelativeLayout.LayoutParams) levelNameView.getLayoutParams();
-            levelNameViewLp.addRule(RelativeLayout.CENTER_HORIZONTAL);
-            levelNameViewLp.addRule(RelativeLayout.ALIGN_PARENT_TOP);
-            RelativeLayout.LayoutParams authorNameViewLp = (RelativeLayout.LayoutParams) authorNameView.getLayoutParams();
-            authorNameViewLp.addRule(RelativeLayout.CENTER_HORIZONTAL);
-            authorNameViewLp.addRule(RelativeLayout.BELOW,levelNameView.getId());
-            RelativeLayout.LayoutParams bestTextViewLp = (RelativeLayout.LayoutParams) bestTextView.getLayoutParams();
-            bestTextViewLp.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
-            bestTextViewLp.addRule(RelativeLayout.CENTER_VERTICAL);
-
-            // Add click functionality
-            level.setClickable(true);
-            final String levelFilenameCpy = "levels/"+levelFilename;
-            level.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent startGame = new Intent(LevelSelect.this, GameActivity.class);
-                    startGame.putExtra("levelFileName",levelFilenameCpy);
-                    startActivity(startGame);
-                }
-            });
-
-            // Add level to the screen
-            layLevels.addView(level);
-        }
+//        boolean even = false;
+//        for (String levelFilename:levelFiles){
+//            // Figure out properties of the level
+//            int highscore = prefHighscores.getInt(FilenameUtils.removeExtension(levelFilename),-1);
+//            Level levelItem = new Level(this,"levels/"+levelFilename);
+//
+//            // Make a container for a level
+//            RelativeLayout level = new RelativeLayout(this);
+//            if (even){
+//                level.setBackgroundColor(getResources().getColor(R.color.levelSelectBackgroundEven));
+//            }
+//            else{
+//                level.setBackgroundColor(getResources().getColor(R.color.levelSelectBackgroundOdd));
+//            }
+//            even = !even;
+//
+//            // Make preview image
+//            ImageView previewView = new ImageView(this);
+//            previewView.setId(View.generateViewId());
+//            previewView.setAdjustViewBounds(true);
+//            try{
+//                Bitmap preview = GameView.getLevelBitmap(levelItem,false);
+//                previewView.setImageBitmap(Bitmap.createScaledBitmap(preview,previewSize,
+//                        preview.getHeight()*previewSize/preview.getWidth(),false));
+//            }
+//            catch (Exception e){
+//                previewView.setImageResource(R.color.colorPrimary);
+//            }
+//
+//            // Make level title text
+//            TextView levelNameView = new TextView(this);
+//            levelNameView.setText(levelItem.getLevelName());
+//            levelNameView.setTextSize(20);
+//            levelNameView.setId(View.generateViewId());
+//            levelNameView.setTypeface(ResourcesCompat.getFont(this,R.font.dtm_mono));
+//
+//            // Make level title text
+//            TextView authorNameView = new TextView(this);
+//            authorNameView.setText(levelItem.getAuthor());
+//            authorNameView.setTextSize(16);
+//            authorNameView.setId(View.generateViewId());
+//            authorNameView.setTypeface(ResourcesCompat.getFont(this,R.font.dtm_mono));
+//
+//            // Make best score text
+//            TextView bestTextView = new TextView(this);
+//            bestTextView.setText(String.format("%s/%s",highscore>=0?highscore:"-",
+//		    levelItem.getBestPossibleScore()>=0?levelItem.getBestPossibleScore():"-"));
+//            bestTextView.setTextSize(16);
+//            bestTextView.setPadding(10,3,10,3);
+//            bestTextView.setId(View.generateViewId());
+//            bestTextView.setTypeface(ResourcesCompat.getFont(this,R.font.dtm_mono));
+//
+//            // Add elements to level container
+//            level.addView(previewView);
+//            level.addView(levelNameView);
+//            level.addView(authorNameView);
+//            level.addView(bestTextView);
+//
+//            // Add layout rules
+//            RelativeLayout.LayoutParams previewViewLp = (RelativeLayout.LayoutParams) previewView.getLayoutParams();
+//            previewViewLp.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
+//            previewViewLp.addRule(RelativeLayout.CENTER_VERTICAL);
+//            previewViewLp.height = max(screenSize.x,screenSize.y)/numLevelsOnScreen;
+//            previewViewLp.width = max(screenSize.x,screenSize.y)/numLevelsOnScreen;
+//            RelativeLayout.LayoutParams levelNameViewLp = (RelativeLayout.LayoutParams) levelNameView.getLayoutParams();
+//            levelNameViewLp.addRule(RelativeLayout.CENTER_HORIZONTAL);
+//            levelNameViewLp.addRule(RelativeLayout.ALIGN_PARENT_TOP);
+//            RelativeLayout.LayoutParams authorNameViewLp = (RelativeLayout.LayoutParams) authorNameView.getLayoutParams();
+//            authorNameViewLp.addRule(RelativeLayout.CENTER_HORIZONTAL);
+//            authorNameViewLp.addRule(RelativeLayout.BELOW,levelNameView.getId());
+//            RelativeLayout.LayoutParams bestTextViewLp = (RelativeLayout.LayoutParams) bestTextView.getLayoutParams();
+//            bestTextViewLp.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+//            bestTextViewLp.addRule(RelativeLayout.CENTER_VERTICAL);
+//
+//            // Add click functionality
+//            level.setClickable(true);
+//            final String levelFilenameCpy = "levels/"+levelFilename;
+//            level.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View v) {
+//                    Intent startGame = new Intent(LevelSelect.this, GameActivity.class);
+//                    startGame.putExtra("levelFileName",levelFilenameCpy);
+//                    startActivity(startGame);
+//                }
+//            });
+//
+//            // Add level to the screen
+//            layLevels.addView(level);
+//        }
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        SharedPreferences prefHighscores = this.getSharedPreferences("Highscores", MODE_PRIVATE);
-        for (int levelId = 0;levelId<layLevels.getChildCount();levelId++){
-            String levelFilename = levelFiles.get(levelId);
-            RelativeLayout level = (RelativeLayout) layLevels.getChildAt(levelId);
-            TextView bestTextView = (TextView) level.getChildAt(3);
-            String bestText = (String) bestTextView.getText();
-            int highscore = prefHighscores.getInt(FilenameUtils.removeExtension(levelFilename),-1);
-            bestText = (highscore>=0?highscore:"-")+"/"+bestText.split("/")[1];
-            bestTextView.setText(bestText);
-        }
+//        SharedPreferences prefHighscores = this.getSharedPreferences("Highscores", MODE_PRIVATE);
+//        for (int levelId = 0;levelId<layLevels.getChildCount();levelId++){
+//            String levelFilename = levelFiles.get(levelId);
+//            RelativeLayout level = (RelativeLayout) layLevels.getChildAt(levelId);
+//            TextView bestTextView = (TextView) level.getChildAt(3);
+//            String bestText = (String) bestTextView.getText();
+//            int highscore = prefHighscores.getInt(FilenameUtils.removeExtension(levelFilename),-1);
+//            bestText = (highscore>=0?highscore:"-")+"/"+bestText.split("/")[1];
+//            bestTextView.setText(bestText);
+//        }
     }
 }
